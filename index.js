a8e2b971099bfac337797b3cbace047684916c8e
miguel <tsambamiguelito@gmail.com>
2021-11-21 16:46:14 +0100

    (refacto) Remove unecessary parenthesis on parameter's function

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const FILE_PARCELS = 'Fichier1.txt';
 const FILE_NUMBER_PARCEL = 'Fichier2.txt';
 const parcel = new Map();
 
-const readContentAndRemoveCarriage = (file) => fs.readFileSync(file, 'utf8').split('\n');
+const readContentAndRemoveCarriage = file => fs.readFileSync(file, 'utf8').split('\n');
 
 const fake = readContentAndRemoveCarriage(FILE_PARCELS).flatMap( bee => bee.split(':'));
 const realParcels = readContentAndRemoveCarriage(FILE_NUMBER_PARCEL).flatMap( bee => bee.split(':'));
